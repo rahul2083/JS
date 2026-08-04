74969d284a5b5d39c57435eb282f04106e220058
rahul2083 <udayjangid3820@gmail.com>
2026-08-04 14:58:56 +0000

    more

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -27,10 +27,36 @@ let myobj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world")
+    // console.log("Hello world")
 }
 
-console.log(typeof(bigNumber)) // bigint
-console.log(typeof(myFunction)) // function
+// console.log(typeof(bigNumber)) // bigint
+// console.log(typeof(myFunction)) // function
 
 // https://262.ecma-internationl.org/5.1/#sec-11.4.3  for documents
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+//  Stack(Primitive), Heap(Non-primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "rahul@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
