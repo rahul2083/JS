0df66d6d0f0a4b19213c45f3aa7593aa65c905bf
rahul2083 <udayjangid3820@gmail.com>
2026-08-04 14:45:20 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive 
+
+// 7 types : String, Number, Boolearn, Null, Undefined, Symbol, BigInt
+
+// const score = 100
+// const score = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+const bigNumber = 564632945638645166136131n
+
+// Reference (Non primitive)
+
+// Array, objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myobj = {
+    name: "Rahul",
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+console.log(typeof(bigNumber)) // bigint
+console.log(typeof(myFunction)) // function
+
+// https://262.ecma-internationl.org/5.1/#sec-11.4.3  for documents
