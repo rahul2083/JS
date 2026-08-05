2574368d23584812c2da2de376b8413da68eb4c6
rahul2083 <udayjangid3820@gmail.com>
2026-08-05 03:59:25 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+const name = "Rahul"
+const repoCount = 20
+
+// console.log(name + repoCount + "why")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`)
+
+const gameName = new String(`rahul-mnc-com`)
+
+// console.log(gameName[4]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('h'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "   rahul   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://google.com//rahul%20sharma"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('rahul'))
+ 
+console.log(gameName.split('-'));
+
