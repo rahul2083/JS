3f5027e30a7b014fbd40ef41e0341b54f00726f5
rahul2083 <udayjangid3820@gmail.com>
2026-08-09 07:51:24 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,30 @@
+ function sayMyname(){
+   console.log("R");
+   console.log("A");
+   console.log("h");
+   console.log("U");
+   console.log("l"); 
+ }
+
+//  sayMyname()
+
+// function addTwoNumbers(number1, number2){
+//   console.log(number1 + number2);
+// }
+
+// addTwoNumbers(3, 5)
+
+function addTwoNumbers(number1, number2){
+//   let result = number1 = number2
+//   return result
+
+   return number1 + number2
+ }
+
+ const result = addTwoNumbers(3, 5)
+
+//  console.log("Result: ", result);
+
+function loginUserMessage(username){
+   retu
+}
