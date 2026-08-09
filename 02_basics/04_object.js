0bc77cd12c585f310907ef37c43262676eff6c0e
rahul2083 <udayjangid3820@gmail.com>
2026-08-09 05:55:35 +0000

    object

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -64,11 +64,44 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+
+const course = {
+    coursename :  "js in hindi",
+    price: "1000",
+    courseInstructor: "Rahul"
+}
+
+const {courseInstructor} = course
+console.log(courseInstructor);
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+// React example Destructuring
+// const navbar = ({commpany}) => {
+
+// }
+
+// navbar(company = "Aplus")
+
+// {
+//     "name" : "rahul",
+//     "coursename": "js",
+//     "price": "free"
+// }
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
+
+ 
