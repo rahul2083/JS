7bca74e5a930e2d37789e98b5aaad6f823f33efe
rahul2083 <udayjangid3820@gmail.com>
2026-08-13 14:18:45 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -25,6 +25,45 @@ function addTwoNumbers(number1, number2){
 
 //  console.log("Result: ", result);
 
-function loginUserMessage(username){
-   retu
+function loginUserMessage(username = "Ram"){
+  if(username === undefined){
+        // console.log("please enter a username");
+        return
+      }
+   return `${username} just loggd in`
 }
+
+// console.log(loginUserMessage("hitesh"));
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(...num1){
+  return num1
+
+}
+
+// console.log(calculateCartPrice(200, 400, 600 ));
+
+const user = {
+  username: "rahul",
+  price: 199
+}
+
+function handleobject(anyobject){
+  console.log(`Username is ${anyobject.username} 
+    and price is ${anyobject.price}`);
+  }
+
+  // handleobject(user)
+handleobject({
+  username: "uday",
+  price: 2000
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
