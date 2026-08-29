cffe70141acdb357e5c3cebda44c02d703d68749
rahul2083 <udayjangid3820@gmail.com>
2026-08-29 14:36:40 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,34 +1,64 @@
-// let a = 10
-// const b = 20
-// var c = 30
+// // // let a = 10
+// // // const b = 20
+// // // var c = 30
 
-let a = 300
-if (true) {
-    let a = 10
-const b = 20
-console.log("INNER: ", a);
+// // let a = 300
+// // if (true) {
+// //     let a = 10
+// // const b = 20
+// // console.log("INNER: ", a);
     
-}
+// // }
 
-console.log(a);
-// console.log(b);
-// console.log(c);
+// // console.log(a);
+// // // console.log(b);
+// // // console.log(c);
 
-// for (let index = 0; index < array.length; index++) {
-//     const element = array[index];
+// // // for (let index = 0; index < array.length; index++) {
+// // //     const element = array[index];
+    
+// // // }
+
+// function one(){
+//     const username = "Rahul"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+    
+//     two()
+// }
+
+// one()
+
+// if (true) {
+//     const username = "RAhul"
+//     if (username === "RAhul") {
+//         const website = "youtube"
+//         console.log(username + website);
+//     }
+
+//     // console.log(website);
     
 // }
 
+// // console.log(username);
 
-function one(){
-    const username = "Rahul"
 
-    function two(){
-        const website = "Youtube"
-        console.log(username);
-        
-    }
-    console.log(website);
+// ++++++++++++++++++++ interesting +++++++++++++
+addone(5)
 
-    two()
+function addone(num){
+    return num + 1
+
 }
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
