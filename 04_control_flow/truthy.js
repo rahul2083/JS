bd30460bb43c5d34a274e6adb160181f9a5fdd78
rahul2083 <udayjangid3820@gmail.com>
2026-09-01 13:54:44 +0000

    flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -36,4 +36,12 @@ let val1;
 // val1 = undefined ?? 15
 val1 = null ?? 10 ?? 15
 
-console.log(val1);
+// console.log(val1);
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
